12af2bf34dc367b3f3bd7198d0eb1ef05c431392
Josh Worden <josh@parameter1.com>
2021-03-08 16:18:02 -0600

    bring in express and helmet

--- services/graphql/src/server.js
+++ services/graphql/src/server.js
@@ -1,10 +1,19 @@
-const { log } = require('@parameter1/terminus/utils');
+const express = require('express');
+const helmet = require('helmet');
 const http = require('http');
+const { log } = require('@parameter1/terminus/utils');
 
-const server = http.createServer((req, res) => {
-  log(`Processing request: ${req.url}`);
-  res.write('hello, world');
-  res.end();
+const app = express();
+app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
+app.use(helmet({ contentSecurityPolicy: false }));
+
+app.get('/', (req, res) => {
+  log('redirecting');
+  res.redirect(301, '/graphql');
 });
 
-module.exports = server;
+app.get('/graphql', (req, res) => {
+  res.send('hello, world.');
+});
+
+module.exports = http.createServer(app);
