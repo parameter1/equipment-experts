793ab7b950a7914dbe2636d749c911f07393ea7c
Josh Worden <josh@parameter1.com>
2021-03-08 16:42:00 -0600

    basic graphql server config

--- services/graphql/src/server.js
+++ services/graphql/src/server.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
 const http = require('http');
-const { log } = require('@parameter1/terminus/utils');
+const { ApolloServer } = require('apollo-server-express');
+const { get, set } = require('@parameter1/utils');
+const { isProduction } = require('./env');
+const schema = require('./schema');
+
+const { STATUS_CODES } = http;
 
 const app = express();
 app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
 app.use(helmet({ contentSecurityPolicy: false }));
 
 app.get('/', (req, res) => {
-  log('redirecting');
   res.redirect(301, '/graphql');
 });
 
-app.get('/graphql', (req, res) => {
-  res.send('hello, world.');
+const path = '/graphql';
+const server = new ApolloServer({
+  schema,
+  tracing: !isProduction,
+  cacheControl: isProduction,
+  introspection: !isProduction,
+  debug: isProduction ? false : { endpoint: path },
+  playground: !isProduction,
+  formatError: (err) => {
+    const code = get(err, 'extensions.exception.statusCode');
+    if (code) set(err, 'extensions.code', STATUS_CODES[code].replace(/\s/g, '_').toUpperCase());
+    return err;
+  },
 });
+server.applyMiddleware({ app, path });
 
 module.exports = http.createServer(app);
