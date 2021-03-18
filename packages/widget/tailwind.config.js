1ad9b2108c6deaccc5f290b445f99d0496994529
Josh Worden <josh@parameter1.com>
2021-03-18 10:45:27 -0500

    Fix tailwind imports, colors, fonts

--- packages/widget/tailwind.config.js
+++ packages/widget/tailwind.config.js
@@ -1,6 +1,6 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const forms = require('@tailwindcss/forms');
 const typography = require('@tailwindcss/typography');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
   plugins: [
@@ -18,14 +18,27 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
       animation: {
         'spin-fast': 'spin 500ms linear infinite',
       },
       colors: {
-        rose: colors.rose,
-        orange: colors.orange,
-        teal: colors.teal,
-        cyan: colors.cyan,
+        'base-blue-main': '#008cba',
+        'base-blue-hover': '#006687',
+        'base-blue-active': '#004b63',
+        'base-blue-border': '#0079a1',
+        'base-blue-border-hover': '#004b63',
+        'base-blue-border-active': '#001921',
+        'base-gray-8': '#ddd', // facet list group item border color
+        'base-gray-7': '#222', // content type facet color
+        'base-gray-6': '#24282c', // left ribbon nav bg color
+        'base-gray-5': '#323a45', // left ribbon nav item bg color
+        'base-gray-4': '#656d70', // left ribbon nav item hover bg color
+        'base-gray-3': '#838383', // content list item text color
+        'base-gray-2': '#999', // result and filter header color
+        'base-gray-1': '#e8e8e8', // result list bg color
       },
     },
   },
